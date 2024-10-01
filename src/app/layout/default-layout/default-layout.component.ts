f107acff87d5a71efeb22da1ebdf3d05e99fb6b8
Ziya Ipek <ziya.ipek@hotmail.com>
2024-10-01 16:03:46 +0300

    Updated sidebar

--- src/app/layout/default-layout/default-layout.component.ts
+++ src/app/layout/default-layout/default-layout.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterOutlet, NavigationEnd } from '@angular/router';
 import { NgScrollbar } from 'ngx-scrollbar';
 import { cilHospital } from '@coreui/icons';
 import { IconDirective, IconComponent } from '@coreui/icons-angular';
@@ -17,13 +17,6 @@ import {
 
 import { DefaultFooterComponent, DefaultHeaderComponent } from './';
 import { navItems } from './_nav';
-
-function isOverflown(element: HTMLElement) {
-  return (
-    element.scrollHeight > element.clientHeight ||
-    element.scrollWidth > element.clientWidth
-  );
-}
 
 @Component({
   selector: 'app-dashboard',
@@ -49,13 +42,50 @@ function isOverflown(element: HTMLElement) {
     IconComponent
   ]
 })
-export class DefaultLayoutComponent {
+export class DefaultLayoutComponent implements OnInit {
   public navItems = navItems;
   icons = { cilHospital };
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.refresh();
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.refresh();
+      }
+    });
+  }
+
+  filterNavItems(): void {
+    const currentUrl = this.router.url;
+
+    if (currentUrl.startsWith('/admin')) {
+      this.navItems = navItems.filter(item =>
+        (typeof item.url === 'string' && item.url.startsWith('/admin')) ||
+        (typeof item.title === 'string' && item.title === 'Admin')
+      );
+    } else if (currentUrl.startsWith('/doctor')) {
+      this.navItems = navItems.filter(item =>
+        (typeof item.url === 'string' && item.url.startsWith('/doctor')) ||
+        (typeof item.title === 'string' && item.title === 'Doctor')
+      );
+    } else if (currentUrl.startsWith('/patient')) {
+      this.navItems = navItems.filter(item =>
+        (typeof item.url === 'string' && item.url.startsWith('/patient')) ||
+        (typeof item.title === 'string' && item.title === 'Patient')
+      );
+    }
+  }
+
+  refresh() {
+    this.filterNavItems();
+  }
+
   onScrollbarUpdate($event: any) {
-    // if ($event.verticalUsed) {
-    // console.log('verticalUsed', $event.verticalUsed);
-    // }
+    if ($event.verticalUsed) {
+      console.log('verticalUsed', $event.verticalUsed);
+    }
   }
 }
